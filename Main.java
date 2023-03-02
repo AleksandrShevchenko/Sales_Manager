c4b93c1893699ebeb4761da2b1a168508535a121
AleksandrShevchenko <region45svet@gmail.com>
2023-03-02 22:35:20 +0500

    Added class SalesManager, Main and file README

--- Main.java
+++ Main.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] price = {20, 30, 10};
+
+        SalesManager sales = new SalesManager(price);
+        System.out.println("Sale max = " + sales.max());
+    }
 }
